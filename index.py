7d75796c20f68914e3849306b414663b66e350e5
Luhao Liu <liuluhao0829@gmail.com>
2018-02-03 22:06:00 +0900

    Update style

--- index.py
+++ index.py
@@ -6,15 +6,4 @@ class IndexHandler(BaseHandler):
 
     @authenticated
     def get(self):
-        username = self.user.username
-        page = {
-            "login": "Chick to login again",
-            "logout": "Chick to logout",
-            "register": "Chick to Register",
-            "/": "Click to go to the index",
-            "chat": "Chick to go to the chat room",
-            "/user/" + username: "Chick to go to the personal page"
-        }
-        self.render("template/index.html",
-                    user=self.user,
-                    urls=page)
+        self.render("template/index.html", user=self.user)

557813d9b7eee625e8cbc22a1ac4c5951f6a62e2
Luhao Liu <liuluhao0829@gmail.com>
2018-02-27 21:32:23 +0900

    automatically create tables

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from tornado.web import RequestHandler
-from base import info_load_from_json, info_write_in_json, Database
+from base import info_load_from_json, info_write_in_json
 from os import path
 import pymysql
 
@@ -40,15 +40,55 @@ class SetupHandler(RequestHandler):
             "cookie_secret": str(info.get("cookie_secret")[0], encoding="utf-8")
         }
         try:
-            database = Database(pymysql.connect(host=str(mysql_info.get("mysql_host")),
-                                                port=int(mysql_info.get("mysql_port")),
-                                                user=str(mysql_info.get("mysql_user")),
-                                                passwd=str(mysql_info.get("mysql_password")),
-                                                database=str(mysql_info.get("database_name"))))
+            database = pymysql.connect(host=str(mysql_info.get("mysql_host")),
+                                       port=int(mysql_info.get("mysql_port")),
+                                       user=str(mysql_info.get("mysql_user")),
+                                       passwd=str(mysql_info.get("mysql_password")),
+                                       database=str(mysql_info.get("database_name")))
         except:
             self.render("template/setup.html", mysql_info=mysql_info, server_info=server_info,
                         warn="Unable to connect to the database")
         else:
+            cursor = database.cursor()
+            create_table_sql = """CREATE TABLE IF NOT EXISTS ws_account(
+                              uid      INT UNSIGNED AUTO_INCREMENT
+                              PRIMARY KEY,
+                              username VARCHAR(25) NOT NULL,
+                              email    VARCHAR(50) NOT NULL,
+                              password VARCHAR(64) NOT NULL,
+                              reg_time DATETIME    NOT NULL,
+                              session  VARCHAR(64) NULL,
+                              CONSTRAINT username
+                              UNIQUE (username),
+                              CONSTRAINT email
+                              UNIQUE (email)
+                              )ENGINE = InnoDB DEFAULT CHARSET=utf8;"""
+            cursor.execute(create_table_sql)
+            create_table_sql = """CREATE TABLE IF NOT EXISTS ws_permission(
+                              uid     INT UNSIGNED AUTO_INCREMENT
+                              PRIMARY KEY,
+                              speak   TINYINT(1) DEFAULT '1' NOT NULL,
+                              connect TINYINT(1) DEFAULT '1' NOT NULL,
+                              gag     TINYINT(1) DEFAULT '0' NOT NULL,
+                              root    TINYINT(1) DEFAULT '0' NOT NULL,
+                              admin   TINYINT(1) DEFAULT '0' NOT NULL,
+                              CONSTRAINT ws_permission_ws_account_uid_fk
+                              FOREIGN KEY (uid) REFERENCES ws_account (uid)
+                              )ENGINE = InnoDB DEFAULT CHARSET=utf8;"""
+            cursor.execute(create_table_sql)
+            create_table_sql = """CREATE TABLE IF NOT EXISTS ws_record(                                
+                              id      BIGINT UNSIGNED AUTO_INCREMENT
+                              PRIMARY KEY,
+                              uid     INT UNSIGNED NOT NULL,
+                              content TEXT         NOT NULL,
+                              date    DATETIME     NULL,
+                              CONSTRAINT ws_record_ws_account_uid_fk
+                              FOREIGN KEY (uid) REFERENCES ws_account (uid)
+                              )ENGINE = InnoDB DEFAULT CHARSET=utf8;"""
+            cursor.execute(create_table_sql)
+            create_table_sql = """CREATE INDEX ws_record_ws_account_uid_fk ON ws_record (uid);"""
+            cursor.execute(create_table_sql)
+            database.commit()
             database.close()
             info_write_in_json(path.join(path.realpath(path.dirname(__file__)), "info/mysql.json"),
                                mysql_info)
